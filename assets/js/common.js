8d9acecb8ae4cbb26b0b0b20808874adcd6262d8
atiqurrahman52 <atiqur.rahman@intelli.global>
2023-02-16 11:19:24 +0600

    related product component done

--- assets/js/common.js
+++ assets/js/common.js
@@ -9,22 +9,29 @@ button.addEventListener('click', () => {
 
 // Image gallery started 
 var grid = document.querySelector('.gallery');
+if(grid){
+
   var masonry = new Masonry(grid, {
     // options
     itemSelector: '.grid-item',
     columnWidth: 200
   });
+}
   // Image gallery end
 
 
 
-  // Highest Selling Courses started
-var swiper8 = new Swiper(".itemsSlider", {
+
+// babyPotato started
+var swiper1 = new Swiper(".babyPotato", {
   spaceBetween: 20,
   slidesPerView: 1,
   loop: true,
+  autoplay: {
+    delay: 2000,
+  },
   pagination: {
-    el: ".itemsSliderPage",
+    el: ".babyPotatoPage",
     clickable: true,
   },
   breakpoints: {
@@ -46,3 +53,38 @@ var swiper8 = new Swiper(".itemsSlider", {
     },
   },
 });
+// babyPotato end
+
+
+
+  // Highest Selling Courses started
+  var swiper8 = new Swiper(".relatedProduct", {
+    spaceBetween: 20,
+    slidesPerView: 1,
+    loop: true,
+    autoplay: {
+      delay: 2000,
+    },
+    pagination: {
+      el: ".relatedProductPage",
+      clickable: true,
+    },
+    breakpoints: {
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 24,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+    },
+  });
