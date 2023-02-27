69ba6a9ebdb9490e0f0a7f48bfe4861d57a7d1c4
atiqurrahman52 <atiqur.rahman@intelli.global>
2023-02-27 18:27:01 +0600

    mobile menu done

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,8 +1,12 @@
 const button = document.querySelector('#menu-button');
 const menu = document.querySelector('#menu');
+const closeMenu = document.querySelector('#close-button');
 
 
 button.addEventListener('click', () => {
+  menu.classList.toggle('hidden');
+});
+closeMenu.addEventListener('click', () => {
   menu.classList.toggle('hidden');
 });
 
@@ -254,59 +258,6 @@ $('.outlets a').click(function(){
 
 
 // Step started 
-
-
-var current_fs, next_fs, previous_fs; 
-var left, opacity, scale;
-var animating; //flag to prevent quick multi-click glitches
-
-$(".next").click(function(){
-  
-  current_fs = $(this).parent();
-  next_fs = $(this).parent().next();
-  
-  //activate next step on progressbar using the index of next_fs
-  $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-  $("#progressbar li").eq($("fieldset").index(current_fs)).addClass("complete");
-  
-  //show the next fieldset
-  next_fs.show(); 
-  //hide the current fieldset with style
-  current_fs.css({'display': 'none'}, {
-    step: function() {
-      next_fs.css({'display': 'block'});
-    }, 
-    complete: function(){
-      current_fs.hide();
-    }, 
-  });
-});
-
-$(".previous").click(function(){
-  
-  current_fs = $(this).parent();
-  previous_fs = $(this).parent().prev();
-  
-  //de-activate current step on progressbar
-  $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-  $("#progressbar li").eq($("fieldset").index(previous_fs)).removeClass("complete");
-  
-  //show the previous fieldset
-  previous_fs.show(); 
-  //hide the current fieldset with style
-  current_fs.css({'display': 'none'}, {
-    step: function() {
-      previous_fs.css({'display': 'block'});
-    }, 
-    complete: function(){
-      current_fs.hide();
-    }, 
-  });
-});
-
-$(".submit").click(function(){
-  return false;
-})
 
 
 
